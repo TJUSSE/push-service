9058235b0d44cb8c2120fc6a2a467d38120a746e
Breezewish <me@breeswish.org>
2015-07-17 10:39:30 +0800

    test

---
+++ push.js
@@ -0,0 +1,29 @@
+var argv = require('optimist').argv;
+var request = require('request');
+
+var hour = null, minute = null;
+var token = 'ceeded729d8c61a44331ed8c7402e354';
+
+if (argv['_'].length > 0) {
+  var p = argv['_'];
+  p = p[0].split(':');
+  var hour = p[0];
+  var minute = p[1];
+}
+
+if (hour == null || minute == null) {
+  hour = 17;
+  minute = 0;
+}
+
+var CronJob = require('cron').CronJob;
+new CronJob('* ' + minute + ' ' + hour + ' * * *', function() {
+  console.log('Schedule...');
+
+  request.get({
+    url: 'http://sse.tongji.edu.cn/sse_subscription/api/get_article/0',
+  }, function (err, res, body) {
+    console.log(body);
+  });
+
+}, null, true, 'Asia/Shanghai');
